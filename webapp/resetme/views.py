7845fb90589fc00b2f04fb100ca043e01800b183
razsm <razsm@volhovec.ru>
2023-11-06 17:22:16 +0300

    work with txt db

--- webapp/resetme/views.py
+++ webapp/resetme/views.py
@@ -19,37 +19,41 @@ def index(request):
         context = {'form': form}
         return render(request, 'index.html', context)
 
+def generate_code():
+    numbers = ['1','2','3','4','5','6','7','8','9']
+    #Generate random code
+    shuffle(numbers)
+    random_code = ''.join([choice(numbers) for n in range(6)])
+    with open('db.txt', 'w') as db:
+        db.write(random_code)
+    #return random_code
     
 def verify_phone(request):
     if search(r'http://127.0.0.1:8000/resetme/',request.META.get('HTTP_REFERER')):
-        # Send code by sms
-        numbers = ['1','2','3','4','5','6','7','8','9']
-        #Generate random code
-        shuffle(numbers)
-        #random_code = ''.join([choice(numbers) for n in range(6)])
-        random_code = randint(100000, 999999)
-        print(f'Отправленный код: {random_code}')
-        print(type(int(random_code)))
-        # api = SmsRuApi()
-        # #Тут необходимо подставить данные из ldap и сгенерировать случайный код
-        # result = api.send_one_sms("+79110400598", random_code)
-        # print(result)
-        submitbutton = request.POST.get("submit")
-        code = ''
-        form = VerifyPhone(request.POST or None)
-        if form.is_valid():
-            code = form.cleaned_data.get("code")
-            print(f"Ваш введенный код: {code}")
-            print(type(int(code)))
-            print(f'Отправленный код: {random_code}')
-            print(type(int(random_code)))
-            if int(random_code) == int(code):
-                context = {'form': form, 'submitbutton': submitbutton}
-                return redirect("success")
-            else:
-                context = {'form': form, 'submitbutton': submitbutton, 'code_error': 'Вы ввели неверный код!'}
-                return render(request, 'verify_phone.html', context)
-        else:
+        if request.method == 'POST':
+            submitbutton = request.POST.get("submit")
+            code = ''
+            form = VerifyPhone(request.POST or None)
+            if form.is_valid():
+                code = form.cleaned_data.get("code")
+                with open('db.txt', 'r') as db:
+                    send_code = db.readlines()[-1].strip('\n')
+                if int(send_code) == int(code):
+                    context = {'form': form, 'submitbutton': submitbutton}
+                    return redirect("success")
+                else:
+                    context = {'form': form, 'submitbutton': submitbutton, 'code_error': 'Вы ввели неверный код!'}
+                    return render(request, 'verify_phone.html', context)
+        elif request.method == 'GET':
+            # # Send code by sms
+            generate_code()
+            # print(f'Отправленный код: {send_code}')
+            # print(type(int(send_code)))
+            # api = SmsRuApi()
+            # #Тут необходимо подставить данные из ldap и сгенерировать случайный код
+            # result = api.send_one_sms("+79110400598", random_code)
+            # print(result)
+            form = VerifyPhone()
             context = {'form': form}
             return render(request, 'verify_phone.html', context)
     else:
