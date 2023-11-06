fa82775fe51d40bbad9058246bf1b57fee6987e9
razsm <razsm@volhovec.ru>
2023-11-06 18:10:08 +0300

    change settings fro postgresql and migrate

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -78,11 +78,22 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+# sqlite
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+# postgresql
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'resetme_db',
+        'USER': 'resetme_user',
+        'PASSWORD': resetme_db_pass,
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
