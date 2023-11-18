40e413e9a1c35aab4de9f24dd0143d559ebff763
razsm <razsm@volhovec.ru>
2023-11-18 18:16:40 +0300

    css style

--- webapp/resetme/forms.py
+++ webapp/resetme/forms.py
@@ -6,12 +6,12 @@ from resetme.models import user
 
 class UserForm(forms.Form):
     username = forms.CharField(required=True, label = "Логин:", max_length=9, strip=True, validators=[RegexValidator(
-        '^[a-z]{5}$|^[a-z]{5}_.{3}$', message="Вы ввели некорректный логин!")], error_messages={'invalid': 'Вы ввели некорректный логин!'}, widget=forms.TextInput(attrs={"class":"form-field"}))
+        '^[a-z]{5}$|^[a-z]{5}_.{3}$', message="Вы ввели некорректный логин!")], error_messages={'invalid': 'Вы ввели некорректный логин!'})
     captcha = CaptchaField(required=True, label='Пожалуйста, введите ответ:', error_messages={'invalid': 'Вы ввели неверный ответ!'})
 
 #DecimalField max_digits=6
 class VerifyPhone(forms.Form):
-    code = forms.CharField(required=False, label="Код подтверждения:", validators=[RegexValidator(
+    code = forms.CharField(required=False, label="", validators=[RegexValidator(
         '^[0-9]{6}$', message="Это не код подтверждения. Не пытайся хитрить.")], error_messages={'invalid': 'Вы ввели неверный код!'})
 
 # Import from model    
